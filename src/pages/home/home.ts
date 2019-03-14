90e19210ac0eb47b84af50968aeace902b6ce5c0
Krisley <krisley.kindermann@jexperts.com.br>
2019-03-14 10:31:15 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private localNotifications: LocalNotifications, private platform: Platform) {
 
   }
 
+  buttonPushClicked() {
+    this.platform.ready().then(() => {
+      this.localNotifications.schedule({
+        title: 'Teste push local notification',
+        text: 'Delayed ILocalNotification',
+        trigger: {at: new Date(new Date().getTime() + 3600)},
+        led: 'FF0000',
+        sound: null
+     });
+    });
+  }
 }
